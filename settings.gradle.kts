88aafa4ae4741a873d896ba27ebafe2b567c0cb4
Alexander Sparkowsky <alexander.sparkowsky@europace.de>
2022-02-28 15:35:07 +0100

    dependencies file

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,6 @@
 rootProject.name = "dependabot-test"
+
+include("moduleA", "moduleB")
+
+project(":moduleA").projectDir = file("subfolder/moduleA")
+project(":moduleB").projectDir = file("subfolder/moduleB")
